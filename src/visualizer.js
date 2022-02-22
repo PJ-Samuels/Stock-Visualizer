ee07e45767fee9a8dd3731d86fbef6ddd29293a3
pjsamuels <osamuels@bu.edu>
2022-02-22 13:17:12 -0500

    plot data

--- src/visualizer.js
+++ src/visualizer.js
@@ -2,13 +2,29 @@
 
 import React, {useState} from 'react';
 import env from 'react-dotenv'
-require('dotenv').config()
+import Plot from 'react-plotly.js';
+
+// require('dotenv').config()
 
 export default function Visualizer(){
+    const [prices, setPrices] = useState([])
+    const [date, setDate] = useState([])
 
-
-
-    function getData(){
+    function cleanData(data){
+        const stock = data["Time Series (Daily)"]
+        let stock_price = []
+        let stock_date = []
+        for (const [key, value] of Object.entries(stock)) {
+            stock_price.push(value['4. close'])
+            stock_date.push(key)
+          }
+        console.log(stock_price)
+        console.log(stock_date)
+        setPrices(stock_price)
+        setDate(stock_date)
+    }
+    function getData(company){
+        console.log(company)
             var axios = require("axios").default;
 
             var options = {
@@ -16,7 +32,7 @@ export default function Visualizer(){
         url: 'https://alpha-vantage.p.rapidapi.com/query',
         params: {
             function: 'TIME_SERIES_DAILY',
-            symbol: 'MSFT',
+            symbol: company,
             outputsize: 'compact',
             datatype: 'json'
         },
@@ -27,14 +43,36 @@ export default function Visualizer(){
         };
         axios.request(options)
             .then(function (response) {
-                console.log(response.data);
+
+                cleanData(response.data)
         })
     }
+    function handleSubmit(event) {
+        event.preventDefault();
+        getData(event.target[0].value)
+      }
     return(
         <div>
             hello
-            <button onclick = {getData()} />
-           
+            <form onSubmit={handleSubmit}> 
+                    Which company do you want to see?
+                    <input type="text"/>
+                    <input type="submit" value="Submit"/>
+            
+            </form>
+            <Plot
+            data={[
+            {
+                x: date, 
+                y: prices,
+                type: 'scatter',
+                mode: 'lines+markers',
+                marker: {color: 'red'},
+            },
+            {type: 'bar', x: [1, 2, 3], y: [2, 5, 3]},
+            ]}
+            layout={ {width: 320, height: 240, title: 'A Fancy Plot'} }
+        />
         </div>
     )
 }
